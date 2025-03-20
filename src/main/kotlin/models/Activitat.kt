30712ef4adf38f1e69cd33937836d3cec0ec52ff
davigo2411 <david.gonzalez.ortega@estudiantat.upc.edu>
2025-03-20 10:35:37 +0100

    feature/actividades: eliminada la funcion crear de actividades

--- src/main/kotlin/models/Activitat.kt
+++ src/main/kotlin/models/Activitat.kt
@@ -20,36 +20,6 @@ class Activitat(
     var dataFi: LocalDateTime,
     var creador: String,
 ) {
-    fun afegirActivitat() {
-        val url = "jdbc:postgresql://nattech.fib.upc.edu:40351/midb"
-        val user = "airplan"
-        val password = "airplan1234"
-
-        val sql = """
-            INSERT INTO activitats (nom, latitud, longitud, datainici, datafi, descripcio, username_creador)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """.trimIndent()
-
-        try {
-            val conn: Connection = DriverManager.getConnection(url, user, password)
-            conn.autoCommit = false
-            val pstmt: PreparedStatement = conn.prepareStatement(sql)
-            pstmt.setString(1, nom)
-            pstmt.setFloat(2, ubicacio.latitud)
-            pstmt.setFloat(3, ubicacio.longitud)
-            pstmt.setTimestamp(4, Timestamp.valueOf(dataInici.toJavaLocalDateTime()))
-            pstmt.setTimestamp(5, Timestamp.valueOf(dataFi.toJavaLocalDateTime()))
-            pstmt.setString(6, descripcio)
-            pstmt.setString(7, creador)
-
-            pstmt.executeUpdate()
-            pstmt.close()
-            conn.commit()
-            conn.close()
-        } catch (e: Exception) {
-            println("Error al afegir l'activitat a la base de dades: ${e.message}")
-        }
-    }
 
     fun modificarActivitat(
         nom: String,
